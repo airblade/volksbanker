38c801293ece3069c5f10e68ac5d8cc23e90687c
Andy Stewart <boss@airbladesoftware.com>
2015-07-08 15:45:30 +0200

    Fix requires.

--- lib/volksbanker.rb
+++ lib/volksbanker.rb
@@ -1,6 +1,9 @@
 require 'volksbanker/version'
 require 'volksbanker/cli'
 require 'volksbanker/volksbank_file_reader'
-require 'volksbanker/volksbank_line_item'
+require 'volksbanker/statement'
+require 'volksbanker/line'
+require 'volksbanker/balance'
+require 'volksbanker/transaction'
 require 'volksbanker/amex_line_item'
 
