f9e13d4f078b8c71fb62638a2d5ba6d6a5b85272
Andy Stewart <boss@airbladesoftware.com>
2015-09-07 15:00:46 +0100

    Handle changed format for opening and closing balances.

--- lib/volksbanker/balance.rb
+++ lib/volksbanker/balance.rb
@@ -5,7 +5,7 @@ module Volksbanker
   class Balance < Line
     attr_reader :amount, :currency, :date
 
-    def initialize(date, _, _, _, _, _, currency, _, amount, credit_or_debit)
+    def initialize(date, _, _, _, _, _, _, _, _, _, currency, amount, credit_or_debit)
       @date = parse_date date
       @currency = currency
       @amount = parse_amount amount, credit_or_debit
